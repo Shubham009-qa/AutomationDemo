1bf10df3f535759c519d2a5f06e264676bb9c289
Shubham009-qa <shubhamsongade91@gmail.com>
2026-05-22 11:11:06 +0530

    added jenkin file

--- utils/env.js
+++ utils/env.js
@@ -1,8 +1,8 @@
 const env = process.env.TEST_ENV || 'DEV';
 
 const urls = {
-    QA: process.env.QAURL,
-    DEV: process.env.DEVURL,
+    STAGE: process.env.STAGEURL,
+    UAT: process.env.UATURL,
     PROD: process.env.PRODURL
 };
 
